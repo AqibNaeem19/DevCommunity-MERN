9282140c4939a4f51850ad626d9990aa8c41b50a
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-06-01 12:00:10 +0500

    Implemented Json Web Token (jwt)

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 // Importing User Model
@@ -59,7 +61,21 @@ router.post('/',
   
 
       //  Return jsonwebtoken
-      res.send('User Registered')
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+      console.log(payload)
+      jwt.sign(
+        payload, 
+        config.get('jwtSecret'),
+        { expiresIn: 360000},
+        (err, token) => {
+          if( err ) throw err;
+          res.json({ token })
+        }
+      )
 
     } catch (err){
       console.error(err.message);
