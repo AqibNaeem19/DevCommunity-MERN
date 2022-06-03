99781af3b5cc6c7033334922d4297fa31b75f946
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-06-03 20:00:42 +0500

    Add education to user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -246,4 +246,62 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/profile/education
+// @desc    Add profile education
+// @access  Private
+router.put(
+  '/education', 
+  [
+    auth, 
+    [
+      check('school', 'School is required').not().isEmpty(),
+      check('degree', 'Company is required').not().isEmpty(),
+      check('fieldofstudy', 'Field of study is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    // If required fields are not present or incorrect, send bad request as a response
+    const errors = validationResult(req);
+    if( !errors.isEmpty()){
+      return res.status(400).json({ errors: errors.array()})
+    }
+
+    //  Destructure the request
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } = req.body
+
+    //  Build experience object
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } 
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      // Push experience object at the start of array
+      profile.education.unshift(newEdu);
+      await profile.save();
+      res.json(profile);
+
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+
+  }
+);
+
 module.exports = router;
