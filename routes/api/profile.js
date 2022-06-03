fe4b69bb53ed31a8f91da677b38049883af0e284
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-06-03 20:14:49 +0500

    Delete education from user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -304,4 +304,23 @@ router.put(
   }
 );
 
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+    // Get index of experience to remove.
+    const removeIndex = profile.education.map(education => education.id).indexOf(req.params.edu_id);
+    profile.education.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile)
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error')
+
+  }
+});
+
 module.exports = router;
