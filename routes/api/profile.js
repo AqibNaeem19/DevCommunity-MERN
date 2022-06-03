d1a100f5717f429e1bedc36bbf011f32447bffce
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-06-03 18:48:51 +0500

    Delete experience from user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -225,6 +225,25 @@ router.put(
     }
 
   }
-)
+);
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+    // Get index of experience to remove.
+    const removeIndex = profile.experience.map(experience => experience.id).indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile)
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error')
+
+  }
+});
 
 module.exports = router;
