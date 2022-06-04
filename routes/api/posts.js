dc205a589e465a66383203c8f1c8174f7ad5b945
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-06-04 12:28:54 +0500

    Get all posts from database

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -45,9 +45,21 @@ router.post(
       res.status(500).send('Server Error');
 
     }
-    
-
-
 });
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1});
+    res.json(posts);
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error')
+  }
+  
+})
 
 module.exports = router;
