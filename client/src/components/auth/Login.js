05caab2449167d2edc57c4d80dce9aed75e8a4ec
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-06-25 12:05:48 +0500

    controlled login form components
    Implement the form for user signin or login with useState hooks

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const Login = () => {
+const Register = () => {
+  const [formData, setFormData] = useState({ 
+    email: '',
+    password: '',
+  });
+
+  // Destructure the object properties
+  const {  email, password } = formData;
+
+  // Takes the name property of the form control and assign the target value
+  const onFormDataChange = event => setFormData({ ...formData, [ event.target.name ]: event.target.value });
+
+  // Form submit handler
+  const onSubmit = event => {
+    event.preventDefault();
+   console.log('Success');
+  }
+
   return (
-    <div>Login</div>
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> SignIn Your Account</p>
+      <form className="form" onSubmit={ e => onSubmit(e) } >
+        
+        <div className="form-group">
+          <input type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange = { e => onFormDataChange(e) }
+            required
+          />
+          
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange = { e => onFormDataChange(e) }
+            minLength="6"
+          />
+        </div>
+       
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
   )
 }
 
-export default Login;
+export default Register;
